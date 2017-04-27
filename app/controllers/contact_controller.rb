a08fa23b9e376352c03868af2e70f72a3450899a
ChHuynh <christopher.huynh.is@gmail.com>
2017-04-27 15:21:01 -0700

    mailer form not working as intended, removed form for now

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,8 +1,21 @@
 class ContactController < ApplicationController
 
-def index
+	def index
+		@contact = Contact.new
+	end
 
+	# def new
+ #    @contact = Contact.new
+ #  end
 
-end
-
+ #  def create
+ #    @contact = Contact.new(params[:contact])
+ #    @contact.deliver
+      # flash.now[:notice] = 'Thank you for your message. I will contact you soon!'
+      # redirect_to "/"
+    # else
+    #   flash.now[:error] = 'Cannot send message.'
+    #  redirect_to "/contact" 
+    # end
+  # end
 end
